e6a725812d90162ad2f07d7623abd93988e2db21
Wonhyeok316 <choewonhyeog3@gmail.com>
2024-01-29 16:11:49 +0900

    sigup_mail with fireabase

--- lib/screens/auth/signup_mail.dart
+++ lib/screens/auth/signup_mail.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:rest_note/screens/auth/nickname.dart';
 // import 'package:rest_note/screens/auth/signup_verify.dart';
 import 'package:rest_note/widgets/submit_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class SignupMailPage extends StatelessWidget {
+class SignupMailPage extends StatefulWidget {
   const SignupMailPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupMailPageState createState() => _SignupMailPageState();
+}
+
+class _SignupMailPageState extends State<SignupMailPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +41,7 @@ class SignupMailPage extends StatelessWidget {
                 width: screenSize.width * 0.72,
                 height: screenSize.height * 0.06,
                 child: TextField(
+                  controller: _emailController,
                   decoration: InputDecoration(
                     prefixIcon: const Icon(Icons.person_outline),
                     hintText: 'Email',
@@ -61,6 +71,7 @@ class SignupMailPage extends StatelessWidget {
                 height: screenSize.height * 0.06,
                 child: TextField(
                   obscureText: true,
+                  controller: _passwordController,
                   decoration: InputDecoration(
                     prefixIcon: const Icon(Icons.lock_outline_rounded),
                     hintText: 'Password',
@@ -84,11 +95,23 @@ class SignupMailPage extends StatelessWidget {
               ),
             ),
             SubmitButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const NicknamePage()),
-                );
+              onPressed: () async {
+                try {
+                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                  );
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const NicknamePage()),
+                  );
+                } on FirebaseAuthException catch (e) {
+                  // 에러 처리: 오류 메시지 표시
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(e.message ?? 'An error occurred')),
+                  );
+                }
               },
               buttonText: 'Sign up',
             ),
@@ -121,5 +144,12 @@ class SignupMailPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 }
