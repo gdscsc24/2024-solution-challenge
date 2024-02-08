53912f082b48857cb2c9249c15823594672cc3b3
Wonhyeok316 <choewonhyeog3@gmail.com>
2024-02-09 00:42:15 +0900

    Auto login fixed

--- lib/screens/auth/splash_screen.dart
+++ lib/screens/auth/splash_screen.dart
@@ -1,7 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:rest_note/screens/auth/intro_slide.dart';
+import 'package:rest_note/screens/auth/auth_complete.dart'; // AuthCompletePage 임포트
+import 'dart:async';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,18 +15,31 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(
-      const Duration(
-          seconds:
-              1), // Set the duration for which the splash screen will be displayed (e.g., 3 seconds)
-      () {
-        // After the specified duration, navigate to the home screen or any other screen
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => IntroSlide(),
-          ),
-        );
-      },
+    _checkAuthentication();
+  }
+
+  void _checkAuthentication() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      // 사용자가 이미 로그인되어 있다면 AuthCompletePage로 바로 이동
+      _navigateToHome();
+    } else {
+      // 사용자가 로그인되어 있지 않다면 일정 시간 후에 IntroSlide로 이동
+      Timer(const Duration(seconds: 2), () {
+        _navigateToIntro();
+      });
+    }
+  }
+
+  void _navigateToHome() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const AuthCompletePage()),
+    );
+  }
+
+  void _navigateToIntro() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => IntroSlide()),
     );
   }
 
@@ -33,12 +47,13 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
-        backgroundColor: const Color(0xFFFAF1F1),
-        body: Center(
-          child: Image.asset(
-            'assets/images/loading_logo.png',
-            width: screenSize.width * 0.78,
-          ),
-        ));
+      backgroundColor: const Color(0xFFFAF1F1),
+      body: Center(
+        child: Image.asset(
+          'assets/images/loading_logo.png',
+          width: screenSize.width * 0.78,
+        ),
+      ),
+    );
   }
 }
