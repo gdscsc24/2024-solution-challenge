3939b36e714b7414984a2b40a391fb10a1dff79a
younaji <younaji22@gm.gist.ac.kr>
2024-02-20 14:13:59 +0900

    like n recommended fix

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -5,12 +5,14 @@ class ProductModel extends ChangeNotifier {
   final String title;
   final String description;
   final String imageLink;
+  final String youtubeLink;
 
   ProductModel({
     required this.contentId,
     required this.title,
     required this.description,
     required this.imageLink,
+    required this.youtubeLink,
   });
 
   factory ProductModel.fromJson(Map<String, dynamic> json) {
@@ -19,6 +21,8 @@ class ProductModel extends ChangeNotifier {
       title: json['title'] ?? 'Default Title',
       description: json['description'] ?? 'Default Description',
       imageLink: json['image_link'] ?? 'https://example.com/default_image.jpg',
+      youtubeLink:
+          json['youtube_link'] ?? 'https://example.com/default_image.jpg',
     );
   }
 }
